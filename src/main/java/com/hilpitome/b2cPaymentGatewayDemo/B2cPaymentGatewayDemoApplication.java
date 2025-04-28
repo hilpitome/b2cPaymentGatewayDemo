5df53d7df1455c48c06af10481fb05cd9b068575
hilary egesa <hilpitome@gmail.com>
2025-04-28 09:47:47 +0300

    create project

---
+++ src/main/java/com/hilpitome/b2cPaymentGatewayDemo/B2cPaymentGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hilpitome.b2cPaymentGatewayDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2cPaymentGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2cPaymentGatewayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hilpitome/b2cPaymentGatewayDemo/B2cPaymentGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilpitome.b2cPaymentGatewayDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2cPaymentGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
